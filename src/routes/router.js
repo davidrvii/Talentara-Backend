a47fcd6613e2cb821f09760da3d4f151509bc451
David Revivaldy <davidrsitorus04@gmail.com>
2025-04-29 20:53:34 +0700

    Setup Starter File

---
+++ src/routes/router.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+
+const userRoutes = require('./userRoutes')
+const talentRoutes = require('./talentRoutes')
+const projectRoutes = require('./projectRoutes')
+const notificationRoutes = require('./notificationRoutes')
+const timelineRoutes = require('./timelineRoutes')
+
+router.get('/', (req, res) => {
+    res.send("Welcome to Talentara")
+})
+
+router.use('/user', userRoutes)
+router.use('/talent', talentRoutes)
+router.user('/project', projectRoutes)
+router.use('/notification', notificationRoutes)
+router.use('/timeline', timelineRoutes)
+
+module.exports = router
