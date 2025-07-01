2b478a3191e88647e3097700c39115705dacb90f
David Revivaldy <davidrsitorus04@gmail.com>
2025-07-01 12:14:41 +0700

    Export Module GetAccessLevel Controller

--- src/controller/projectController.js
+++ src/controller/projectController.js
@@ -264,6 +264,7 @@ module.exports = {
     getProjectOrder,
     getProjectDetail,
     getCurrentProject,
+    getAccessLevel,
     createNewProject,
     updateProject,
     respondToProjectOffer,
