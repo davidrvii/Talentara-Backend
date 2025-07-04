e92a34830a37339cc97d2890d66bbed463c18699
David Revivaldy <davidrsitorus04@gmail.com>
2025-07-05 00:03:23 +0700

    Add multer to handle image

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const userController = require('../controller/userController')
 const { authentication, authorization } = require('../middleware/auth')
+const upload = multer();
 
 router.get('/admin', userController.getAllUser)
 
@@ -12,7 +13,7 @@ router.post('/register', userController.userRegister)
 
 router.post('/login', userController.userLogin)
 
-router.patch('/update/:id', authentication, authorization, userController.updateUser)
+router.patch('/update/:id', authentication, authorization, upload.single('user_image'), userController.updateUser)
 
 router.patch('/fcm', authentication, authorization, userController.saveFcmToken)
 
