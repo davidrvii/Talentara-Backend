ba8ca4ee1b8c70433e2589488e9a5c9b5b28b458
David Revivaldy <davidrsitorus04@gmail.com>
2025-07-10 14:28:31 +0700

    Change firebase env to base64

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,10 @@
 const admin = require("firebase-admin")
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64
+if (!base64) throw new Error("FCM ENV not set")
+
+const jsonString = Buffer.from(base64, "base64").toString("utf8")
+const serviceAccount = JSON.parse(jsonString)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
